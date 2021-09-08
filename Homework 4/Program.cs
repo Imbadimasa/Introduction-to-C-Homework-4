56f44cb9607ba51777e5d8fa44deb82ca3af066a
Imbadimasa <chalganovdima@gmail.com>
2021-09-07 22:27:53 -0400

    Homework 4.2 terrible solution, please show me how can I improve it.

--- Homework 4/Program.cs
+++ Homework 4/Program.cs
@@ -6,7 +6,41 @@ namespace Homework_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the numbers, use space between the numbers.");
+
+            /*
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             Никак не могу понять как искать двух-, трехзначное число в string. Подскажите пожалуста.
+             */
+
+            //There must be a better way. If you show me the solution, it will be much appreciated.
+
+            int sum = extractSum();
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        static int extractSum()
+        {
+
+            string input = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    double digit = Char.GetNumericValue(input[i]);
+                    sum = sum + Convert.ToInt32(digit);
+                }
+            }
+            return sum;
         }
     }
 }
