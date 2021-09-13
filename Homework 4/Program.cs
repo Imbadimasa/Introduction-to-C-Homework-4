9a1d246975cf29820b975cd37cba0a5e61c72045
Imbadimasa <chalganovdima@gmail.com>
2021-09-12 22:01:26 -0400

    Homework 5.3

--- Homework 4/Program.cs
+++ Homework 4/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
 
 namespace Homework_4
 {
@@ -6,7 +11,21 @@ namespace Homework_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = "Hello World!";
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);     //List the bytes used for data storage
+            }
+
+            string writtenBytes = "Bytes.txt";
+            File.WriteAllBytes(writtenBytes, data);
+
+            byte[] readBytes = File.ReadAllBytes(writtenBytes);
+            Console.WriteLine(Encoding.ASCII.GetString(readBytes));
+
+
+            Console.ReadLine();
         }
     }
 }
