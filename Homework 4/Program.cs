65a625330153a9b1820ab5648c9a91823f93fbba
Imbadimasa <chalganovdima@gmail.com>
2021-09-15 23:06:59 -0400

    Homework 6. Very interesting

--- Homework 4/Program.cs
+++ Homework 4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 
 namespace Homework_4
 {
@@ -6,7 +8,60 @@ namespace Homework_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Process[] myProcess = Process.GetProcesses();
+
+            for (int i = 0; i < myProcess.Length; i++)
+            {
+                Console.WriteLine();
+                Console.Write(myProcess[i]);
+                Console.Write($" {myProcess[i].Id}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            bool loop = true;
+            Console.WriteLine("Please choose a process you want to kill.");
+
+            while (loop == true)
+            {
+                string chosenProcess = Console.ReadLine();
+                int processID;
+                bool success = int.TryParse(chosenProcess, out processID);
+
+                if (success)
+                {
+                    for (int i = 0; i < myProcess.Length; i++)
+                    {
+                        if (myProcess[i].Id == processID)
+                        {
+                            myProcess[i].Kill();
+                            Console.WriteLine("Successful Kill!");
+                            loop = false;
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < myProcess.Length; i++)
+                    {
+                        if (Convert.ToString(myProcess[i]) == chosenProcess)
+                        {
+                            myProcess[i].Kill();
+                            Console.WriteLine("The kill was successful!");
+                            loop = false;
+                            break;
+                        }
+                    }
+                    if (loop)
+                    {
+                        Console.WriteLine("Please enter the correct name/ID");
+                    }
+                }
+
+            }
+            Console.ReadLine();
         }
     }
 }
